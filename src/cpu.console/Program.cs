0ee3a5754ea65f7901c038b27c98b1cc507df7eb
Nicolas Lebrun <nicolas.lebrun95@gmail.com>
2023-09-01 22:00:34 +0200

    Start with cpu model

--- src/cpu.console/Program.cs
+++ src/cpu.console/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using cpu;
+
+Cpu cpu = new();
+
+
+Console.ReadLine();
--- src/cpu/Cpu.cs
+++ src/cpu/Cpu.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace cpu
 {
-    internal class Cpu
+    public class Cpu
     {
+        /// <summary>
+        /// Instruction register
+        /// </summary>
+        public byte IR { get; set; }
+
+        /// <summary>
+        /// Accumulator register
+        /// </summary>
+        public byte A { get; set; }
+
+        /// <summary>
+        /// X register
+        /// </summary>
+        public byte X { get; set; }
+
+        /// <summary>
+        /// Y register
+        /// </summary>
+        public byte Y { get; set; }
+
+        /// <summary>
+        /// Processor status
+        /// </summary>
+        public byte P { get; set; }
+
+        /// <summary>
+        /// Program counter register
+        /// </summary>
+        public UInt16 PC { get; set; }
+
+        /// <summary>
+        /// Stack pointer register
+        /// </summary>
+        public byte S { get; set; }
+
+        public Cpu()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            IR = 0;
+            A = 0;
+            X = 0;
+            Y = 0;
+            P = 0;
+            PC = 0xFFFC;
+            S = 0;
+        }
     }
 }
